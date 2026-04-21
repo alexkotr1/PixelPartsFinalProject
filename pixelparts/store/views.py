8232d42f8fef466cf3951d01948342c71540f5bb
Alex Kotrotsios <alexkotrotsios123@gmail.com>
2026-04-21 14:53:35 +0300

    more catalogue filters

--- pixelparts/store/views.py
+++ pixelparts/store/views.py
@@ -45,6 +45,9 @@ def register(request):
 def catalogue(request):
     category_id = request.GET.get('category')
     searched = request.GET.get('search_text')
+    brand = request.GET.get('brand')
+    price_start = request.GET.get('price_start')
+    price_end = request.GET.get('price_end')
 
     products = Product.objects.all()
 
@@ -52,19 +55,42 @@ def catalogue(request):
         category_id = int(category_id)
         products = products.filter(category_id=category_id)
 
+    if brand:
+        products = products.filter(brand=brand)
+
+    if price_start:
+        products = products.filter(price__gte=price_start)
+    if price_end:
+        products = products.filter(price__lte=price_end)
+
     if searched:
         hits = []
         for product in products:
+            search_terms = searched.lower().strip().split(" ")
             if searched.lower() in product.name.lower() or searched.lower() in product.brand.lower():
                 hits.append(product)
+
+            elif len(search_terms) > 0:
+                for term in search_terms:
+                    if term in product.name.lower() or term in product.brand.lower() and product not in hits:
+                        hits.append(product)
+
         products = hits
 
     categories = Category.objects.all()
+    brands = Product.objects.values_list('brand', flat=True).distinct()
+    print(list(brands))
+
     return render(request, 'store/catalogue.html', {
         'products': products,
         'categories': categories,
         'selected_category_id': category_id,
         'search_text': "" if (searched and searched == "") or searched is None else searched,
+        'price_start': price_start,
+        'price_end': price_end,
+        'brand': brand,
+        'brands': brands,
+
     })
 
 
