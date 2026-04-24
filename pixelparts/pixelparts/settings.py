e7771179f06e2231c52a6996cd4656575abf4a1d
Alex Kotrotsios <alexkotrotsios123@gmail.com>
2026-04-25 01:53:12 +0300

    deploying is fun vol 3

--- pixelparts/pixelparts/settings.py
+++ pixelparts/pixelparts/settings.py
@@ -120,6 +120,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
